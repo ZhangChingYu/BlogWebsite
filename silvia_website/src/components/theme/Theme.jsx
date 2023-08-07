04d6c229c09f246b3cfbf24a06f876185624b597
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-07 17:47:42 +0800

    theme temp

--- silvia_website/src/components/theme/Theme.jsx
+++ silvia_website/src/components/theme/Theme.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import './theme.css';
+import { motion } from "framer-motion";
 
 const Theme = () => {
     return(
-        <div id="theme">Theme</div>
+        <div id="theme" className="theme">
+            <div className="theme_dragon">
+                <motion.div className="theme_dragon_circle"  animate={{x:"5rem", boxShadow:"var(--box-shadow)"}} transition={{duration:1, type:"tween"}}>
+                </motion.div>
+            </div>
+            <div className="theme_cat">
+                <motion.div className="theme_cat_circle" animate={{x:"-5rem", boxShadow:"var(--box-shadow)"}} transition={{duration:1, type:"tween"}} >
+                </motion.div>
+            </div>
+        </div>
     )
 }
 
