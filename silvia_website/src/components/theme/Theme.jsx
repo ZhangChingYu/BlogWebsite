dae7de9134ec7b108ed2a6a75dcadf166843a749
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-07 17:52:26 +0800

    theme temp

--- silvia_website/src/components/theme/Theme.jsx
+++ silvia_website/src/components/theme/Theme.jsx
@@ -6,11 +6,11 @@ const Theme = () => {
     return(
         <div id="theme" className="theme">
             <div className="theme_dragon">
-                <motion.div className="theme_dragon_circle"  animate={{x:"5rem", boxShadow:"var(--box-shadow)"}} transition={{duration:1, type:"tween"}}>
+                <motion.div className="theme_dragon_circle"  animate={{x:"5rem", boxShadow:"4px 0px 0px 0px rgba(0, 0, 0, 0.25)"}} transition={{duration:1, type:"tween"}} initial={{boxShadow:"0px 4px 4px 0px rgba(0, 0, 0, 0.25)"}}>
                 </motion.div>
             </div>
             <div className="theme_cat">
-                <motion.div className="theme_cat_circle" animate={{x:"-5rem", boxShadow:"var(--box-shadow)"}} transition={{duration:1, type:"tween"}} >
+                <motion.div className="theme_cat_circle" animate={{x:"-5rem", boxShadow:"-4px 0px 0px 0px rgba(0, 0, 0, 0.25)"}} transition={{duration:1, type:"tween"}} initial={{boxShadow:"0px 4px 4px 0px rgba(0, 0, 0, 0.25)"}}>
                 </motion.div>
             </div>
         </div>
