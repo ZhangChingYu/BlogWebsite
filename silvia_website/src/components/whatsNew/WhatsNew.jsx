06b80dc075a4aa401dbdb5cb19b3c10e8527f198
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-07 18:49:05 +0800

    what's new

--- silvia_website/src/components/whatsNew/WhatsNew.jsx
+++ silvia_website/src/components/whatsNew/WhatsNew.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import './whatsNew.css';
+import imgUrl from '../../assets/cat.png';
+import { motion } from 'framer-motion';
 
 
 const WhatsNew = () => {
     return(
-        <div>What's New</div>
+        <div id="latest" className="whatsnew">
+            <div className="whatsnew_title">
+                <h1>What's New</h1>
+            </div>
+            <div className="whatsnew_container">
+                <motion.div className="whatsnew_container_card" animate={{marginTop:"-2rem"}} transition={{duration:2, type:"tween"}} >
+                    <div className="whatsnew_container_card_r1">
+                        <h2>Welcome To My Website!!</h2>
+                        <p>2023/07/21</p>
+                    </div>
+                    <div className="whatsnew_container_card_r2">
+                        <p>This is a website that shares my daily life and some things that I do to improve my self.
+The website is divided into two themes: Work and Life</p>
+                    </div>
+                    <div className="whatsnew_container_card_r3">
+                        <div className="whatsnew_container_card_tag">
+                            <p>#Life</p>
+                            <p>#Work</p>
+                        </div>
+                        <p>Read More</p>
+                    </div>
+                </motion.div>
+                <div className="whatsnew_container_pic">
+                    <img src={imgUrl} alt="Welcome To My Website"/>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
