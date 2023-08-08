05a4e553051ce611c1b9a159dfd0fecfc097e03d
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-08 16:33:37 +0800

    go back navigator

--- silvia_website/src/components/navbar/Navbar.jsx
+++ silvia_website/src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css';
 import imgUrl from '../../assets/logo.png';
 import { motion } from "framer-motion";
 
-const Navbar = () =>{
+const Navbar = ({ navigator }) =>{
     return(
         <div className="m_navbar">
             <motion.div 
@@ -11,7 +11,7 @@ const Navbar = () =>{
             animate={{top:0 ,boxShadow:"0px 4px 0 -1px rgba(0, 0, 0, 0.25)"}} 
             transition={{duration:1, type:'tween'}} 
             initial={{top:-10}}
-            onClick={()=>{console.log("click")}}>
+            onClick={()=>{navigator(-1)}}>
                 <motion.img src={imgUrl} alt="back to home"/>
             </motion.div>
         </div>
--- silvia_website/src/pages/about/About.jsx
+++ silvia_website/src/pages/about/About.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import './about.css';
 import { Navbar } from '../../components'
 import { Intro, CatIntro, DragonIntro } from '../../components/about';
-import {Footer } from '../../containers';
+import { Footer } from '../../containers';
+import { useNavigate } from 'react-router-dom';
 
 const About = () => {
+    const navigator = useNavigate();
+
     return(
         <div className="about">
-            <Navbar/>
+            <Navbar navigator={navigator}/>
             <Intro/>
             <DragonIntro/>
             <CatIntro/>
