e1787927dd83c3dc814a8fe006a68a05b4a968c7
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-08 16:31:05 +0800

    about footer

--- silvia_website/src/components/navbar/Navbar.jsx
+++ silvia_website/src/components/navbar/Navbar.jsx
@@ -6,7 +6,12 @@ import { motion } from "framer-motion";
 const Navbar = () =>{
     return(
         <div className="m_navbar">
-            <motion.div className="navbar_circle" animate={{top:0 ,boxShadow:"0px 4px 0 -1px rgba(0, 0, 0, 0.25)"}} transition={{duration:1, type:'tween'}} initial={{top:-10}}>
+            <motion.div 
+            className="navbar_circle" 
+            animate={{top:0 ,boxShadow:"0px 4px 0 -1px rgba(0, 0, 0, 0.25)"}} 
+            transition={{duration:1, type:'tween'}} 
+            initial={{top:-10}}
+            onClick={()=>{console.log("click")}}>
                 <motion.img src={imgUrl} alt="back to home"/>
             </motion.div>
         </div>
--- silvia_website/src/containers/footer/Footer.jsx
+++ silvia_website/src/containers/footer/Footer.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import './footer.css';
+import { FaFacebook, FaLine} from 'react-icons/fa'
+import { RiInstagramFill } from 'react-icons/ri'
+import { AiFillTwitterCircle } from 'react-icons/ai'
 
 const Footer = () => {
     return(
-        <div>Footer</div>
+        <div className="m_footer">
+            <div className="m_footer_content">
+                <div className="m_footer_subscribe">
+                    <h2>If You Would Like To Subscribe</h2>
+                    <p>You can leave your email down below so we can notified you whenever there is a new post being updated.</p>
+                    <input placeholder="ex. 123456@gmail.com"/>
+                    <div className="m_footer_subscribe_btn">
+                        <p>Subscribe</p>
+                    </div>
+                </div>
+                <div className="m_footer_contact">
+                    <h2>Get In Touch</h2>
+                    <p>If you have something you want to share with me, you can send an email to the website’s mail box</p>
+                    <p className="m_footer_contact_email">MySite@gmail.com</p>
+                    <p>Welcome to share your own coding experience or things happen in your life with me.</p>
+                    <div className="m_footer_contact_media">
+                        <FaFacebook className="m_footer_contact_media_icon" size={26}/>
+                        <RiInstagramFill className="m_footer_contact_media_icon" size={26}/>
+                        <AiFillTwitterCircle className="m_footer_contact_media_icon" size={26}/>
+                        <FaLine className="m_footer_contact_media_icon" size={26} />
+                    </div>
+                </div>
+            </div>
+            <p>Copyright©2023 SilviaChang.</p>
+        </div>
     )
 }
 
