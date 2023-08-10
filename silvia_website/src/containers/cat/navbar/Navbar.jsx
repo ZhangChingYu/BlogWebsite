d11e42d397982c352965d9d53f39f0af00a8866d
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-10 11:19:46 +0800

    navigator cat to home

--- silvia_website/src/containers/cat/navbar/Navbar.jsx
+++ silvia_website/src/containers/cat/navbar/Navbar.jsx
@@ -4,13 +4,15 @@ import { Menu, Search } from '../../../components/cat';
 import imgUrl from '../../../assets/footprint.png';
 import { motion } from 'framer-motion';
 
-const Navbar = () =>{
+const Navbar = ({navigator}) =>{
     const [searchInput, setSearchInput] = useState("");
     console.log(searchInput);
     return(
         <div className="cat_navbar">
             <Menu/>
-            <motion.div animate={{y:-20, opacity:1}} initial={{opacity:0.3}} transition={{duration:0.3, type:"tween"}} className="cat_navbar_b">
+            <motion.div 
+            animate={{y:-20, opacity:1}} initial={{opacity:0.3}} transition={{duration:0.3, type:"tween"}} 
+            className="cat_navbar_b" onClick={()=>{navigator(-1)}}>
                 <div className="cat_navbar_back">
                     <img src={imgUrl} alt="Back To Home"/>
                 </div>
--- silvia_website/src/pages/cat/Cat.jsx
+++ silvia_website/src/pages/cat/Cat.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import './cat.css';
 import { Navbar, Header, Category, Latest} from '../../containers/cat';
 import { Footer } from '../../containers';
+import { useNavigate } from 'react-router-dom';
 
 const Cat = () =>{
+    const navigator = useNavigate();
+
     const background = "var(--color-cat-bg)";
     return(
         <div className="cat">
-            <Navbar/>
+            <Navbar navigator={navigator}/>
             <Header />
             <Latest Title={"Latest"} PageLink={'/life/latest'}/>
             <Latest Title={"Highlights"} PageLink={'/life/highlight'}/>
