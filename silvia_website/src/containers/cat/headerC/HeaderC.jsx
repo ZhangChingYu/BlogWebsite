897ab052f03b58f6d17da35d7634f775b5c5cd6d
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-11 15:21:16 +0800

    cat header C

--- silvia_website/src/containers/cat/headerC/HeaderC.jsx
+++ silvia_website/src/containers/cat/headerC/HeaderC.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import './headerC.css';
+import { PiShareFatFill } from 'react-icons/pi';
 
 const HeaderC = () => {
     return(
-        <div>Article Header C</div>
+        <div className="cat_headerC" >
+            <div className="cat_headerC_content">
+                <div className="cat_headerC_header">
+                    <p className="cat_headerC_date">2023/08/02</p>
+                    <PiShareFatFill size={25} className="cat_headerC_share"/>
+                </div>
+                <h2>Celebrating Father’s Day by Baking a Special Cake.</h2>
+                <p>Whenever there is something to celebrate the baking energy inside me starts erupting. The picture was took 4 years ago, I still remember I took a lot of effort on that cake because I want my father to have a Father’s Day he would never forget. <br/><br/>In August, 2023, the upcoming Father's Day has many kids in a bind, especially those who are having trouble choosing a gift. Yet, nothing is more heart warming than a self-made gift, so why don’t make it tastier?</p>
+            </div>
+        </div>
     )
 }
 
--- silvia_website/src/pages/catArticle/CatArticle.jsx
+++ silvia_website/src/pages/catArticle/CatArticle.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './catArticle.css';
-import { ArticleNavbar, ArticleHeaderA, ArticleSectionA } from '../../containers/cat';
+import { ArticleNavbar, ArticleHeaderC, ArticleSectionA } from '../../containers/cat';
 import { Footer } from '../../containers';
 
 const CatArticle = () => {
     return(
         <div className="cat_article">
             <ArticleNavbar />
-            <ArticleHeaderA />
+            <ArticleHeaderC />
             <ArticleSectionA />
             <Footer />
         </div>
