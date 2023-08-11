7870fb74b255b0ca84f945936a4df4965e26e538
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-11 14:28:01 +0800

    cat header A

--- silvia_website/src/containers/cat/headerA/HeaderA.jsx
+++ silvia_website/src/containers/cat/headerA/HeaderA.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import './headerA.css';
-import imgUrl from '../../../assets/cakecover.JPG';
+import imgUrl from '../../../assets/dragon.png';
 import { PiShareFatFill } from 'react-icons/pi';
 
 
 const HeaderA = () => {
     return(
         <div className="cat_headerA">
-            <div className="cat_headerA_img" >
-                <img src={imgUrl} alt=""/>
+            <div className="cat_headerA_share">
+                <PiShareFatFill className="cat_headerA_share_icon" size={25} />
             </div>
-            <div className="cat_headerA_content">
-                <div className="cat_headerA_content_share">
-                    <PiShareFatFill size={25} className="cat_headerA_content_share_icon"/>
-                </div>
-                <h2>Celebrating Father’s Day by Baking a Special Cake.</h2>
-                <p className="cat_header_content_date">2023/08/02</p>
-                <p>Whenever there is something to celebrate the baking energy inside me starts erupting. The picture was took 4 years ago, I still remember I took a lot of effort on that cake because I want my father to have a Father’s Day he would never forget. <br/><br/>In August, 2023, the upcoming Father's Day has many kids in a bind, especially those who are having trouble choosing a gift. Yet, nothing is more heart warming than a self-made gift, so why don’t make it tastier?</p>
+            <h2>Celebrating Father’s Day by Baking a Special Cake.</h2>
+            <p className="cat_headerA_date">2023/08/02</p>
+            <p>Whenever there is something to celebrate the baking energy inside me starts erupting. The picture was took 4 years ago, I still remember I took a lot of effort on that cake because I want my father to have a Father’s Day he would never forget. <br/><br/>In August, 2023, the upcoming Father's Day has many kids in a bind, especially those who are having trouble choosing a gift. Yet, nothing is more heart warming than a self-made gift, so why don’t make it tastier?</p>
+            <div className="cat_headerA_img">
+                <img src={imgUrl} alt=""/>
             </div>
         </div>
     )
--- silvia_website/src/pages/catArticle/CatArticle.jsx
+++ silvia_website/src/pages/catArticle/CatArticle.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import './catArticle.css';
-import { ArticleNavbar, ArticleHeaderB, ArticleSectionA } from '../../containers/cat';
+import { ArticleNavbar, ArticleHeaderA, ArticleSectionA } from '../../containers/cat';
 import { Footer } from '../../containers';
 
 const CatArticle = () => {
     return(
         <div className="cat_article">
             <ArticleNavbar />
-            <ArticleHeaderB />
+            <ArticleHeaderA />
             <ArticleSectionA />
             <Footer />
         </div>
