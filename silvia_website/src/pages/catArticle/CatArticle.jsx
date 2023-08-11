7e054056df0cb9be283c3942530aaeb4435f97d3
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-11 19:48:45 +0800

    cat section C

--- silvia_website/src/pages/catArticle/CatArticle.jsx
+++ silvia_website/src/pages/catArticle/CatArticle.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './catArticle.css';
 import { ArticleNavbar, ArticleHeaderA, ArticleSectionC } from '../../containers/cat';
 import { Footer } from '../../containers';
+import { CateItem } from "../../components/cat";
 
 const CatArticle = () => {
     return(
@@ -9,6 +10,12 @@ const CatArticle = () => {
             <ArticleNavbar />
             <ArticleHeaderA />
             <ArticleSectionC />
+            <h2 className="cat_article_category_text">Related Categories:</h2>
+            <div className="cat_article_categories">
+                <CateItem name={"Baking"} />
+                <CateItem name={"Daily"} />
+                <CateItem name={"Experience"} />
+            </div>
             <Footer />
         </div>
     )
