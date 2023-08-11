1a454aad5dff68d4c5e533856adb6ea25a3ebe2a
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-11 11:33:52 +0800

    keyword show

--- silvia_website/src/pages/search/Search.jsx
+++ silvia_website/src/pages/search/Search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import './search.css';
 import { Navbar } from '../../components';
 import { SearchItem } from '../../components/home';
@@ -6,20 +6,36 @@ import { Footer } from '../../containers';
 import { useNavigate } from "react-router-dom"; 
 import { PageNext } from '../../components/cat';
 
-const Search = ({keyword}) => {
+const Search = () => {
     const navigator = useNavigate();
+    const [input, setInput] = useState("");
+    const [keyword, setKeyword] = useState(" ");
+    const searchHandler = () => {
+        if(input===" "){
+            setKeyword("");
+            console.log("e")
+        }
+        else {
+            setKeyword(input);
+        }
+    }
     return(
         <div className="home_search">
             <Navbar navigator={navigator}/>
             <div className="home_search_box">
-                <input placeholder=""/>
-                <div className="home_search_btn">
+                <input 
+                onKeyPress={(event)=>{if(event.code==="Enter"){searchHandler()}}}
+                type={"text"} 
+                ref={(element) => element?.focus?.()} 
+                placeholder={""} 
+                onChange={(event)=>{setInput(event.target.value)}}/>
+                <div className="home_search_btn" onClick={searchHandler}>
                     <p>Search</p>
                 </div>
             </div>
             <div className="home_search_key">
-                <h2 style={{visibility:keyword!==undefined?"hidden":"unset"}}>The search result of: </h2>
-                <p style={{visibility:keyword!==undefined?"hidden":"unset"}}>Key word</p>
+                <h2 style={{visibility:keyword===""?"hidden":"visible"}}>The search result of: </h2>
+                <p style={{visibility:keyword===""?"hidden":"visible"}}>{keyword}</p>
             </div>
             <div className="home_search_result">
                 <SearchItem />
