8e82d7728f6907b2b224993da69ed09cdc9fda5e
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-11 21:18:41 +0800

    cat category button navigation

--- silvia_website/src/components/cat/cateItem/CateItem.jsx
+++ silvia_website/src/components/cat/cateItem/CateItem.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './cateItem.css';
+import { useNavigate } from "react-router-dom";
 
 const CateItem = ({name}) =>{
+    const navigator = useNavigate();
     return(
-        <div className="cat_cateItem">
+        <div className="cat_cateItem" onClick={()=>{navigator("/life/category", {state:{category:name}})}}>
             <p>{name}</p>
         </div>
     )
--- silvia_website/src/components/home/whatsNew/WhatsNew.jsx
+++ silvia_website/src/components/home/whatsNew/WhatsNew.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './whatsNew.css';
 import imgUrl from '../../../assets/cat.png';
 import { motion } from 'framer-motion';
+import { Link } from "react-router-dom";
 
 
 const WhatsNew = () => {
@@ -17,15 +18,14 @@ const WhatsNew = () => {
                         <p>2023/07/21</p>
                     </div>
                     <div className="whatsnew_container_card_r2">
-                        <p>This is a website that shares my daily life and some things that I do to improve my self.
-The website is divided into two themes: Work and Life</p>
+                        <p>This is a website that shares my daily life and some things that I do to improve my self. The website is divided into two themes: Work and Life</p>
                     </div>
                     <div className="whatsnew_container_card_r3">
                         <div className="whatsnew_container_card_tag">
                             <p>#Life</p>
                             <p>#Work</p>
                         </div>
-                        <p>Read More</p>
+                        <Link to={"/life/article"} style={{textDecorationLine:"none", color:"#616161"}}><p>Read More</p></Link>
                     </div>
                 </motion.div>
                 <div className="whatsnew_container_pic">
