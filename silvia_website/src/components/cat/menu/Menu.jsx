5b63b15704a3c790b4ee51359e4223ef6ffb8df4
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-12 12:10:05 +0800

    cat menu adjustment

--- silvia_website/src/components/cat/menu/Menu.jsx
+++ silvia_website/src/components/cat/menu/Menu.jsx
@@ -15,13 +15,13 @@ const Menu = () => {
                 <motion.div animate={{opacity:showMenu?0:1}} transition={{duration:0.3, type:"tween"}} className="cat_menu_middle"></motion.div>
                 <motion.div animate={{rotate:showMenu?-45:0, y:showMenu?-10:0}} transition={{duration:0.3, type:"tween"}} className="cat_menu_bottom"></motion.div>
             </div>
-            {showMenu?<motion.div animate={{scaleY:1, opacity:1}} initial={{scaleY:0, opacity:0.2}} transition={{duration:0.4, type:"tween"}} className="cat_menu_content">
+            <motion.div animate={{height:showMenu?"210px":"0px", opacity:showMenu?1:0}} initial={{height:0, opacity:0}} transition={{duration:0.4, type:"tween"}} className="cat_menu_content">
                 <Link to={"/"} style={{textDecoration:"none"}}><motion.p whileHover={{scale:1.1}} whileTap={{scale:0.95}}>Home</motion.p></Link>
                 <Link to={"/life/latest"} state={{title:"Latest"}} style={{textDecoration:"none"}}><motion.p whileHover={{scale:1.1}} whileTap={{scale:0.95}}>Latest</motion.p></Link>
                 <Link to={"/life/latest"} state={{title:"Highlights"}} style={{textDecoration:"none"}}><motion.p whileHover={{scale:1.1}} whileTap={{scale:0.95}}>Highlights</motion.p></Link>
                 <Link to="/life/categories" style={{textDecoration:"none"}}><motion.p whileHover={{scale:1.1}} whileTap={{scale:0.95}}>Category</motion.p></Link>
                 <Link to={"/"} style={{textDecoration:"none"}}><motion.p whileHover={{scale:1.1}} whileTap={{scale:0.95}}>Work</motion.p></Link>
-            </motion.div>:<></>}
+            </motion.div>
         </div>
     )
 }
