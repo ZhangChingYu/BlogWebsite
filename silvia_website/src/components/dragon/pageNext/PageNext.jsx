297d6dd08fa16ebbbc706f22610cb1d979fe15df
Silvia Chang <sunny1218.chang@gmail.com>
2023-08-13 21:26:25 +0800

    dragon page next item

--- silvia_website/src/components/dragon/pageNext/PageNext.jsx
+++ silvia_website/src/components/dragon/pageNext/PageNext.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import './pageNext.css';
+import {MdNavigateNext, MdNavigateBefore } from 'react-icons/md';
 
 const PageNext = () => {
+    const [preState, setPreState] = useState(false);
+    const [nextState, setNextState] = useState(true);
+    const [atPage, setAtPage] = useState(1);
+
+    const nextPageHandler = () => {
+        if(atPage<3){
+            setAtPage(atPage+1);
+        }
+    }
+    const prePageHandler = () => {
+        if(atPage>1){
+            setAtPage(atPage-1);
+        }
+    }
     return(
-        <div>Dragon Page Next</div>
+        <div className="dragon_page_next">
+            <MdNavigateBefore size={40} className="dragon_page_next_" color={preState?"var(--color-dragon-item)":"#969696"} />
+            <div className="dragon_page_next_numbers">
+                <div className="dragon_page_next_number" style={{background:atPage===1?"var(--color-dragon-item)":"#ffffff00"}}>
+                    <p style={{color:atPage===1?"var(--color-dragon-theme)":"var(--color-dragon-item)"}}>1</p>
+                </div>
+                <div className="dragon_page_next_number" style={{background:atPage===2?"var(--color-dragon-item)":"#ffffff00"}}>
+                    <p style={{color:atPage===2?"var(--color-dragon-theme)":"var(--color-dragon-item)"}}>2</p>
+                </div>
+                <div className="dragon_page_next_number" style={{background:atPage===3?"var(--color-dragon-item)":"#ffffff00"}}>
+                    <p style={{color:atPage===3?"var(--color-dragon-theme)":"var(--color-dragon-item)"}}>3</p>
+                </div>
+            </div>
+            <MdNavigateNext size={40} className="dragon_page_next_" color={nextState?"var(--color-dragon-item)":"#969696"} onClick={nextPageHandler}/>
+        </div>
     )
 }
 
