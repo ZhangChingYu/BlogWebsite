a015f04de1741d77195ed760bdead7b6ad9bb92d
Silvia Chang <sunny1218.chang@gmail.com>
2025-02-22 19:10:37 +1100

    cat theme animation

--- silvia_website/src/components/home/theme/Theme.jsx
+++ silvia_website/src/components/home/theme/Theme.jsx
@@ -6,12 +6,15 @@ import { useNavigate } from "react-router-dom";
 import Lottie from "lottie-react";
 import WorkShow from "../../../lottie/Simple Work Theme-2.json";
 import WorkType from "../../../lottie/Work Theme Type-2.json";
+import LifeShow from "../../../lottie/Life Simple.json";
+import LifeType from "../../../lottie/life theme-2.json";
 
 const Theme = () => {
     const navigator = useNavigate();
     const [dragonHover, setDragonHover] = useState(false);
     const [catHover, setCatHover] = useState(false);
     const [dragonEnd, setDragonEnd] = useState(false);
+    const [lifeEnd, setLifeEnd] = useState(false);
     return( 
         <div id="theme" className="theme">
             <div className="theme_dragon" title="check out my work" onClick={()=>{navigator("/work")}} onPointerEnter={()=>{setDragonHover(true)}} onPointerLeave={()=>{setDragonHover(false)}}>
@@ -24,14 +27,22 @@ const Theme = () => {
                 <div className="theme_dragon_btn"> 
                     <p>My Work</p>
                 </div>
-                <Link to={'/work'} className="theme_dragon_link">
-                    <motion.div className="theme_dragon_circle" animate={{x:"5rem", boxShadow:"4px 0px 0px 0px rgba(0, 0, 0, 0.25)"}} transition={{duration:1, type:"tween"}} initial={{boxShadow:"0px 4px 4px 0px rgba(0, 0, 0, 0.25)"}}>
-                    </motion.div>
-                </Link>
-                
             </div>
             <div className="theme_cat" title="check out my life" onClick={()=>{navigator("/life")}} onPointerEnter={()=>{setCatHover(true)}} onPointerLeave={()=>{setCatHover(false)}}>
-                <motion.p className="theme_cat_text" animate={{opacity:catHover?0.95:0}} initial={{opacity:0}} transition={{duration:0.5, type:"tween"}}>Check out my Life</motion.p>
+                
+                <div className="theme_animate">
+                    {lifeEnd?
+                        <Lottie animationData={LifeType} loop={true} autoPlay={false}/>:
+                        <Lottie animationData={LifeShow} loop={false} autoPlay={false} onComplete={()=>{setLifeEnd(true)}}/>
+                    }
+                </div>
+                <div className="theme_cat_btn"> 
+                    <p>My Life</p>
+                </div>
+                <Link to={'/work'} className="theme_dragon_link">
+                    <motion.div className="theme_dragon_circle" animate={{x:"-5rem", boxShadow:"4px 0px 0px 0px rgba(0, 0, 0, 0.25)"}} transition={{duration:1, type:"tween"}} initial={{x:"-10rem", boxShadow:"0px 4px 4px 0px rgba(0, 0, 0, 0.25)"}}>
+                    </motion.div>
+                </Link>
                 <Link to={'/life'} className="theme_cat_link">
                     <motion.div className="theme_cat_circle" animate={{x:"-5rem", boxShadow:"-4px 0px 0px 0px rgba(0, 0, 0, 0.25)"}} transition={{duration:1, type:"tween"}} initial={{boxShadow:"0px 4px 4px 0px rgba(0, 0, 0, 0.25)"}}>
                     </motion.div>
